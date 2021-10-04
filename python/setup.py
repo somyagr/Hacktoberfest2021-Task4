6a95ec7814beb5b34b7a93e2a54c3f9d25fefa0f
somyagr <78006502+somyagr@users.noreply.github.com>
2021-10-04 12:34:33 +0530

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -221,7 +221,7 @@ def macosx_sdk_specified():
     global MACOS_SDK_SPECIFIED
 
     # If already called, return cached result.
-    if MACOS_SDK_SPECIFIED;
+    if MACOS_SDK_SPECIFIED:
         return MACOS_SDK_SPECIFIED
 
     # Find the sdk root and set MACOS_SDK_SPECIFIED
@@ -229,7 +229,7 @@ def macosx_sdk_specified():
     return MACOS_SDK_SPECIFIED
 
 
-def is_macosx_sdk_path(path);
+def is_macosx_sdk_path(path):
     """
     Returns True if 'path' can be located in a macOS SDK
     """
